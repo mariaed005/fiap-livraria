6d9e0b0a795b8a0039bf0ad50ebbae52b2a75174
Logon PF Fiap <logonpflocal@fiap.com.br>
2024-03-15 09:53:27 -0300

    Construtores e relacionamento entre classes

--- src/CriarLivros.java
+++ src/CriarLivros.java
@@ -4,23 +4,29 @@ public class CriarLivros {
         //Instanciando um objeto => criar o objeto na memória
         // tipoObjeto nomeObjeto = operadorInstanciação  construtor;
         //   Livro      favorito =       new               Livro();
-        Livro favorito = new Livro();
-        favorito.titulo = "O Hobbit";
+        //Instanciar editora
+        Editora editora = new Editora();
+        editora.nome = "Harper Collins";
+        editora.representante = "Joao";
+                Livro favorito = new Livro("O Hobbit");
+        //favorito.titulo = "O Hobbit";
         favorito.autor = "J. R. R. Talkien";
         favorito.valor = 37.27;
-        favorito.editora = "Harper Collins";
+        favorito.editora = editora;
         favorito.resumo = "Bilbo era um dos ...";
         favorito.tipoCapa = "COMUM";
 
         System.out.println("Meu livro favorito é:" +
         favorito.titulo + "\nSeu autor é: " + favorito.autor +
         " preço: R$ " + favorito.valor);
+        System.out.println(favorito.exibirDados());
 
-        Livro meuLivro = new Livro();
-        meuLivro.titulo = "UML 2.0";
+        Livro meuLivro = new Livro("UML 2.0",5);
+        //meuLivro.titulo = "UML 2.0";
         meuLivro.valor = 58.98;
-        System.out.println("\n\nTitulo: " + meuLivro.titulo);
-        System.out.println("Autor: " + meuLivro.autor);
-
+        meuLivro.editora=editora;
+        //System.out.println("\n\nTitulo: " + meuLivro.titulo);
+        //System.out.println("Autor: " + meuLivro.autor);
+        System.out.println(meuLivro.exibirDados());
     }
 }
--- src/Livro.java
+++ src/Livro.java
@@ -5,12 +5,44 @@ public class Livro {
     public String titulo;
     public String autor;
     public double valor;
-    public String editora;
+    public Editora editora;
     public int paginas;
     public String resumo;
     public String tipoCapa;
 
 
     // --- MÉTODOS ---
+    public String exibirDados(){
+        String dados =
+                "---------------" +
+                "\nTitulo: " + this.titulo +
+                "\nResumo: " + this.resumo +
+                "\nPreço: R$ " + this.valor +
+                "\nPáginas: " + this.paginas +
+                "\nAutor: " + this.autor +
+                "\nEditora:" + this.editora.nome +
+                "\n---------------" ;               ;
+        return dados;
+    }
+    public Livro(String titulo){
+        this.titulo = titulo;
+    }
 
+    public Livro(){}
+    public Livro(String titulo, double preco){
+        this.titulo = titulo;
+        this.valor = preco;
+    }
+    public Livro(String titulo, String autor,
+                 double valor, Editora editora,
+                 int paginas, String resumo,
+                 String tipoCapa) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.valor = valor;
+        this.editora = editora;
+        this.paginas = paginas;
+        this.resumo = resumo;
+        this.tipoCapa = tipoCapa;
+    }
 }
